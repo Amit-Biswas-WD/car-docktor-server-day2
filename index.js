435c7fc115892333d85fc9f1ea800d329fce6251
Amit biswas <amitbiswas9282@gmail.com>
2025-02-14 21:34:44 +0600

    single product

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.POST || 5000;
@@ -28,6 +28,17 @@ async function run() {
     app.get("/services", async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 0, service_id: 1 },
+      };
+      const result = await servicesCollection.findOne(query, options);
       res.send(result);
     });
 
